e8ea349e61e1b44cc520ef63505ec5c84b86c662
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2023-08-10 10:20:16 +0300

    Update

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -4,10 +4,10 @@ import {
   Avatar,
   Name,
 } from './FriendListItem.styled';
-export const FriendListItem = ({ avatar, name, isOnline }) => {
+export const FriendListItem = ({ avatar, name, onlineStatus }) => {
   return (
-    <StyledFriendListItem isOnline={isOnline}>
-      <StatusIndicator isOnline={isOnline}></StatusIndicator>
+    <StyledFriendListItem isOnline={onlineStatus}>
+      <StatusIndicator isOnline={onlineStatus}></StatusIndicator>
       <Avatar src={avatar} alt={`${name}'s avatar`} width="48" />
       <Name>{name}</Name>
     </StyledFriendListItem>
